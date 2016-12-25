4b84994d4714985baceafea6a14cc00e9991d344
arnav <arnavgoel2@gmail.com>
2016-12-25 02:03:53 -0500

    Update Histogram.py

--- Histogram.py
+++ Histogram.py
@@ -54,3 +54,36 @@ for (chan, color) in zip(chans, colors):
 # to be application dependent
 print ("flattened feature vector size: %d" % (np.array(features).flatten().shape))
  
+#Similarly you can plot 2d Histograms as well 
+# let's move on to 2D histograms -- I am reducing the
+# number of bins in the histogram from 256 to 32 so we
+# can better visualize the results
+fig = plt.figure()
+ 
+# plot a 2D color histogram for green and blue
+ax = fig.add_subplot(131)
+hist = cv2.calcHist([chans[1], chans[0]], [0, 1], None,
+	[32, 32], [0, 256, 0, 256])
+p = ax.imshow(hist, interpolation = "nearest")
+ax.set_title("2D Color Histogram for Green and Blue")
+plt.colorbar(p)
+ 
+# plot a 2D color histogram for green and red
+ax = fig.add_subplot(132)
+hist = cv2.calcHist([chans[1], chans[2]], [0, 1], None,
+	[32, 32], [0, 256, 0, 256])
+p = ax.imshow(hist, interpolation = "nearest")
+ax.set_title("2D Color Histogram for Green and Red")
+plt.colorbar(p)
+ 
+# plot a 2D color histogram for blue and red
+ax = fig.add_subplot(133)
+hist = cv2.calcHist([chans[0], chans[2]], [0, 1], None,
+	[32, 32], [0, 256, 0, 256])
+p = ax.imshow(hist, interpolation = "nearest")
+ax.set_title("2D Color Histogram for Blue and Red")
+plt.colorbar(p)
+ 
+# finally, let's examine the dimensionality of one of
+# the 2D histograms
+print "2D histogram shape: %s, with %d values" % (hist.shape, hist.flatten().shape[0])
