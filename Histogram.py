7f24a5b732d42d8abf72a4ad69b4543be96644b8
arnav <arnavgoel2@gmail.com>
2016-12-25 01:13:38 -0500

    Create Histogram.py

---
+++ Histogram.py
@@ -0,0 +1,16 @@
+# import the necessary packages
+from matplotlib import pyplot as plt
+import numpy as np
+import argparse
+import cv2
+ 
+# construct the argument parser and parse the arguments
+#Know more about argparse 
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required = True, help = "Path to the image")
+args = vars(ap.parse_args())
+ 
+# load the image and show it
+image = cv2.imread(args["image"])
+cv2.imshow("image", image)
