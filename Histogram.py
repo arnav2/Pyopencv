7628b08802fe4cb32b28c40e1c8d41022c0fb077
arnav <arnavgoel2@gmail.com>
2016-12-25 01:57:24 -0500

    Update Histogram.py

--- Histogram.py
+++ Histogram.py
@@ -15,3 +15,35 @@ args = vars(ap.parse_args())
 # load the image and show it
 image = cv2.imread(args["image"])
 cv2.imshow("image", image)
+
+#Simple conversion to grayscale and then making a Histogram 
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+cv2.imshow("gray", gray)
+hist = cv2.calcHist([gray], [0], None, [256], [0, 256])
+plt.figure()
+plt.title("Grayscale Histogram")
+plt.xlabel("Bins")
+plt.ylabel("# of Pixels")
+plt.plot(hist)
+plt.xlim([0, 256])
+#Chans is a 1*3 matrix where the first column is for red then for green and next for blue 
+chans = cv2.split(image)
+colors = ("b", "g", "r")
+plt.figure()
+plt.title("'Flattened' Color Histogram")
+plt.xlabel("Bins")
+plt.ylabel("# of Pixels")
+features = []
+for (chan, color) in zip(chans, colors):
+	# create a histogram for the current channel and
+	# concatenate the resulting histograms for each
+	# channel
+	hist = cv2.calcHist([chan], [0], None, [256], [0, 256])
+	features.extend(hist)
+ 
+	# plot the histogram
+	plt.plot(hist, color = color)
+	plt.xlim([0, 256])
+ 
+ print ("flattened feature vector size: %d" % (np.array(features).flatten().shape))
+ 
